425087d1389e6b0eff0e42b86ff9fbd4717a8a9d
unknown <s-salma.mahran@zewailcity.edu.eg>
2023-03-29 12:19:09 +0200

    modify model.py

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,4 @@
+
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
@@ -8,40 +9,7 @@ import datetime
 db = SQLAlchemy()
 
 
-class Users(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-    name = db.Column(db.String(150), nullable=False)
-    # created_at = db.Column(db.DateTime(timezone=True),server_default=func.now())
-    # User Data
-
-
-class UserLocations(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.ForeignKey(Users.id), nullable=False)
-
-
-class UserMembers(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.ForeignKey(Users.id), nullable=False)
-    location_id = db.Column(db.ForeignKey(UserLocations.id), nullable=False)
-
-
-class Media(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(150), unique=True, nullable=False)
-    location_id = db.Column(db.ForeignKey(UserLocations.id))
-    member_id = db.Column(db.ForeignKey(UserMembers.id))
-    type = db.Column(db.String(), nullable=False)
-    user_id = db.Column(db.ForeignKey(Users.id), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now())
-
-
-class AnalysisResults(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
