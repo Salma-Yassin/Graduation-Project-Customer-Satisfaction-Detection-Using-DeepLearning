f7e70abcb7cbbe6b9735d6a7d1e51258d9de8dfa
ahmeedmaahmoud <s-ahmedabdelmoneim@zewailcity.edu.eg>
2023-04-17 01:57:21 +0200

    new updatesssssssssssssss

--- apps/inferenceflask.py
+++ apps/inferenceflask.py
@@ -1,9 +1,7 @@
 import requests
 from apps import app
 from .TestEmotionDetector import predictEmotionFace
-from .yolo_inference import functionpaths_image
-from .yolo_inference import functionpaths_video
-import json
+
 API_URL = "https://api-inference.huggingface.co/models/superb/hubert-large-superb-er"
 headers = {"Authorization": "Bearer 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"}
 
@@ -12,7 +10,7 @@ def query(file_url):
     cont = requests.get(file_url, verify= False)
     response = requests.post(API_URL, headers=headers, data=cont, verify= False).json()
     
-    return response[0]['label']
+    return response
 
 
 @app.route('/face_inference')
@@ -20,10 +18,3 @@ def query_face(file_url):
     # cont = requests.get(file_url, verify= False)
     response = predictEmotionFace(file_url)
     return response
-
-@app.route('/body_inference')
-def query_body(file_url):
-    emotion=functionpaths_video(file_url)
-    emotion=sorted(emotion.items(), key=lambda x:x[1],reverse=True)
-    output=json.dumps(emotion,indent=4)
-    return output
