1b206a4420a2f10b2a2e781c3ea83fcf17ab5840
unknown <s-salma.mahran@zewailcity.edu.eg>
2023-04-17 00:54:00 +0200

    add some files and changing our inference.py to inferenceflask.py

---
+++ apps/inferenceflask.py
@@ -0,0 +1,20 @@
+import requests
+from apps import app
+from .TestEmotionDetector import predictEmotionFace
+
+API_URL = "https://api-inference.huggingface.co/models/superb/hubert-large-superb-er"
+headers = {"Authorization": "Bearer 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"}
+
+@app.route('/tone_inference')
+def query(file_url):
+    cont = requests.get(file_url, verify= False)
+    response = requests.post(API_URL, headers=headers, data=cont, verify= False).json()
+    
+    return response[0]['label']
+
+
+@app.route('/face_inference')
+def query_face(file_url):
+    # cont = requests.get(file_url, verify= False)
+    response = predictEmotionFace(file_url)
+    return response
